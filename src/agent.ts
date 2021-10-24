cb79b6cc31f2e74ea2387dd75a690ad6995ba71e
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-24 18:16:10 -0400

    Stateless version

--- src/agent.ts
+++ src/agent.ts
@@ -6,6 +6,7 @@ import {
   getJsonRpcUrl,
   FindingSeverity,
   FindingType,
+  HandleBlock,
 } from "forta-agent";
 import { TimeRangeStore } from "./store";
 import {
@@ -13,6 +14,7 @@ import {
   COMPTROLLER_ABI,
   COMPTROLLER_ADDR,
   CTOKEN_ABI,
+  TIME_WINDOW,
 } from "./utils";
 
 const getUtilizationRate = async (
@@ -32,6 +34,7 @@ const getUtilizationRate = async (
   return borrows.div(cash);
 };
 
+// this method assumes that handleBlock is run sequentially on blocks, it's not documented wether or not this is the way that agents are run. According to https://github.dev/forta-protocol/forta-agent-sdk/blob/eec16a9b1b1be697dbf124e11b3602fca839a6e0/cli/commands/run/run.live.ts#L17-L34, they are run asyncroniously on blocks. See the next method for an alternative stateless implementation.
 const provideHandleBlock = () => {
   const ctokenStores: {
     [addr: string]: TimeRangeStore;
@@ -90,6 +93,53 @@ const provideHandleBlock = () => {
   };
 };
 
+// stateless version
+const handleBlock: HandleBlock = async (blockEvent) => {
+  const findings: Finding[] = [];
+
+  const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+  const comptroller = new ethers.Contract(
+    COMPTROLLER_ADDR,
+    COMPTROLLER_ABI,
+    provider
+  );
+  const ctokenAddrs: string[] = await comptroller.getAllMarkets({
+    blockTag: blockEvent.blockNumber,
+  });
+  const ctokens = ctokenAddrs.map(
+    (addr) => new ethers.Contract(addr, CTOKEN_ABI, provider)
+  );
+  for (const ctoken of ctokens) {
+    const name: string = await ctoken.name({
+      blockTag: blockEvent.blockNumber,
+    });
+
+    const currRate = await getUtilizationRate(ctoken, blockEvent.blockNumber);
+
+    let block = await provider.getBlock(blockEvent.blockNumber);
+    while (blockEvent.block.timestamp - block.timestamp >= TIME_WINDOW) {
+      const prevRate = await getUtilizationRate(ctoken, block.number);
+      if (currRate.minus(prevRate).abs().div(prevRate).gte(CHANGE_FRAC)) {
+        findings.push(
+          Finding.fromObject({
+            name: "cToken Utilization Rate changed",
+            description: `cToken ${name} Utilization Rate changed by more than 10%.`,
+            alertId: "COMPOUND_CTOKEN_UTILIZATION_RATE",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+          })
+        );
+        break;
+      }
+
+      if (block.number == 0) break;
+      block = await provider.getBlock(block.number - 1);
+    }
+  }
+
+  return findings;
+};
+
 export default {
-  handleBlock: provideHandleBlock(),
+  handleBlock,
 };
