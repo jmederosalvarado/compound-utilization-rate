0e4effe2b264ea22df12813a8fa8f760571fa187
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-24 15:16:02 -0400

    Fix timestamp calculation

--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,12 @@ import BigNumber from "bignumber.js";
 
 export const COMPTROLLER_ADDR = "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b";
 export const COMPTROLLER_ABI = [
-  "function getAllMarkets() public view returns (uint)",
+  "function getAllMarkets() public view returns (address[])",
 ];
 export const CTOKEN_ABI = [
-  "function totalBorrowsCurrent() public view returns (uint)",
-  "function totalSupply() public view returns (uint)",
+  "function name() public view returns (string)",
   "function getCash() public view returns (uint)",
+  "function totalBorrowsCurrent() public view returns (uint)",
 ];
 
 export class TimeRangeStore {
@@ -17,10 +17,10 @@ export class TimeRangeStore {
     let max = new BigNumber(0);
     for (const timestampKey in this.store) {
       const timestampStored = parseInt(timestampKey);
-      if (timestamp - timestampStored >= 60 * 1000) {
-        delete this.store[timestamp];
-      } else if (this.store[timestamp].gt(max)) {
-        max = this.store[timestamp];
+      if (timestamp - timestampStored >= 60 * 60) {
+        delete this.store[timestampKey];
+      } else if (this.store[timestampKey].gt(max)) {
+        max = this.store[timestampKey];
       }
     }
     this.store[timestamp] = rate;
